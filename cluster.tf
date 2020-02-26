781ee26fe548c58c36587b0db0d8181d3cc7f697
Zachary Seguin <zachary@zacharyseguin.ca>
2020-02-25 21:31:21 -0500

    feat(gateway): Configure for gateway mode

--- cluster.tf
+++ cluster.tf
@@ -5,33 +5,16 @@ resource "ibm_container_cluster" "cluster" {
 
   kube_version = "${var.kubernetes_version}"
 
-  # public_vlan_id  = "${data.ibm_network_vlan.public.id}"
+  public_vlan_id  = "${data.ibm_network_vlan.public.id}"
   private_vlan_id = "${data.ibm_network_vlan.private.id}"
 
+  public_service_endpoint  = false
   private_service_endpoint = true
+
+  gateway_enabled = true
 
   machine_type      = "${var.node_size}"
   hardware          = "shared"
   default_pool_size = "${var.node_count}"
   disk_encryption   = true
-}
-
-resource "ibm_container_worker_pool" "edge" {
-  worker_pool_name = "pool1"
-  cluster          = "${ibm_container_cluster.cluster.id}"
-  machine_type     = "${var.edge_node_size}"
-  size_per_zone    = "${var.edge_node_count}"
-  hardware         = "shared"
-  disk_encryption  = true
-  labels = {
-    dedicated = "edge"
-  }
-}
-
-resource "ibm_container_worker_pool_zone_attachment" "edge" {
-  cluster         = "${ibm_container_cluster.cluster.id}"
-  worker_pool     = "${element(split("/",ibm_container_worker_pool.edge.id),1)}"
-  zone            = "${var.datacenter}"
-  public_vlan_id  = "${data.ibm_network_vlan.public.id}"
-  private_vlan_id = "${data.ibm_network_vlan.private.id}"
 }
